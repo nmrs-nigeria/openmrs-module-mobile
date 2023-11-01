59b78d2d70578c23a508e3318db59b14690c4a15
Gbenge Aondoakula Raphael <agbenge@cihpng.org>
2023-11-01 12:04:36 +0100

    refactored code

--- PbsProcessor/src/pbsprocessor/listerner/GenericResponseCallbackListener.java
+++
@@ -1,9 +0,0 @@
-package pbsprocessor.listerner;
-
-public interface GenericResponseCallbackListener<T> {
-
-    //void onResponse();
-    void onResponse(T obj);
-    void onErrorResponse(T errorMessage);
-    void onErrorResponse(String errorMessage);
-}
